7f226a280bab812708629115c84370342b7f464b
Erick Henrique <erick.henrique@aluno.ufabc.edu.br>
2021-02-16 11:30:43 -0300

    Finalizado

---
+++ Forca/Assets/Comentarios.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comentarios : MonoBehaviour
+{
+    /*Script unicamente para explicar a customização pedida nos labs 
+     * Lab1: Tela inicial identificando o jogo: Para identificar o jogo fui até a cena Lab1_start e no Game Object canvas criei um objeto text e mudei seu texto para jogo da forca e ajustei seu tamanho, cor e altura
+     * Lab1: musica do jogo: Baixei uma musica na internet que achei que combinaria com o jogo, criei um Game Object, adicionei um audio source ao object e vinculei a musica ao AudioClip e selecionei a opção loop na musica para que ela não pare, por fim criei um script que movia o objeto a Scene dontDestroyOnLoad para que quando a cena fosse trocada a musica não parasse
+     * Lab2: Para criar um fim com credito criei uma nova cena com um canvas, nesse canvas criei 3 objetos text, um agradecendo por jogar, outro com nome e outro com ra, por fim criei um botão que quando clicado executa uma função que fecha o jogo, por fim nas cenas de start, enforcado e salvo eu coloquei um botão que levava para a cena dos creditos
+     */
+    
+}
